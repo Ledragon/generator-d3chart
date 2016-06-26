32b759e240efbed162fa459d9747588c994247d1
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-06-26 18:09:15 +0200

    WIP

--- app/index.js
+++ app/index.js
@@ -1,23 +1,47 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+        this.argument('appName', { type: String, required: true });
+    },
     writing: {
         packageJSON: function () {
-            this.copy('_package.json', 'package.json');
+            this.fs.copyTpl(
+                this.templatePath('_package.json'),
+                this.destinationPath('package.json'),
+                {
+                    packageName: _.kebabCase(this.appName)
+                });
         },
         git: function () {
             this.copy('_.gitignore', '.gitignore');
-
         },
         html: function () {
-            this.copy('_index.html', 'index.html');
+            this.fs.copyTpl(
+                this.templatePath('_index.html'),
+                this.destinationPath('index.html'),
+                {
+                    appName: this.appName
+                }
+            );
         },
         assets: function () {
-            // var source = this.templatePath('_styles.css');
-            // var destination = this.destinationPath('styles.css');
             this.copy('_styles.css', 'styles.css');
+        },
+        scripts: function () {
+            this.copy('_app.js', 'app.js');
+        },
+        typings: function () {
+            this.fs.copyTpl(
+                this.templatePath('_typings.json'),
+                this.destinationPath('typings.json'),
+                {
+                    packageName: _.kebabCase(this.appName)
+                });
         }
     }
 });
